16853436b4fb07210f4fe504aa2e65a25b409dcc
DHONDGE YASHVARDHAN CHATURBHUJ <yashvardhandhondgepc@outlook.com>
2025-01-12 20:43:27 +0530

    Update index.js

--- pdf_analyzer/Backend/index.js
+++ pdf_analyzer/Backend/index.js
@@ -8,7 +8,7 @@ import dotenv from "dotenv"
 dotenv.config()
 const app = express();
 const corsOptions = {
-    origin: process.env.FRONTEND_URL, 
+    origin: "*", 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
     credentials: true, 
   };
